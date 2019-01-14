47cd6057874c43486c364d5328e6895ffc2e8f5d
Deepak Acharya <aacharyadeepak100@gmail.com>
2019-01-14 20:30:49 +0530

    Updated Docstring

--- index.py
+++ index.py
@@ -3,9 +3,13 @@
 place="Hebri"
 
 def changeMadu():
+    """
+    Checking Local/Global Scope
+    """
     global place ## Global Scope
     place="Hanglur"
     print(place)
 
 changeMadu()
 print(place)
+
