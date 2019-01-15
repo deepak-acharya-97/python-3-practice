a142b917d56a81eeb530e41320fb3665322ce87f
Deepak Acharya <aacharyadeepak100@gmail.com>
2019-01-15 11:44:26 +0530

    Created sample instances and overrridden __str__ for object display

--- oo.py
+++ oo.py
@@ -1,12 +1,16 @@
 class UselessFriend:
     
-    UselessFriend.Friends=[]
+    Friends=[]
 
     def __init__(self, name, age, place, rating):
         self.name=name
         self.age=age
         self.place=place
-        self.rating=rating
+        self.rating=rating ## Rating is for friendship level out of 10
         UselessFriend.Friends.append(self)
 
-    
+uselessFriend1=UselessFriend('AAA',23,"Hanglur",8)
+uselessFriend2=UselessFriend('BBB',22,"Koteshwara",8.1)
+
+for friend in UselessFriend.Friends:
+    print(friend)
